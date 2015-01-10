902ef0936fe5d9d40f9448cb39a1fef1fcda6efa
Kyle Corbitt <kylecorbitt@gmail.com>
2015-01-10 16:47:44 +0000

    all assets included and ring dep removed

--- project.clj
+++ project.clj
@@ -1,55 +1,60 @@
 (defproject word-dream "0.1.0-SNAPSHOT"
  :description "Make words appear on the screen"
  :dependencies [[org.clojure/clojure "1.6.0"]
-                [lib-noir "0.8.4"]
-                [ring-server "0.3.1"]
-                [selmer "0.6.8"]
-                [com.taoensso/timbre "3.2.1"]
-                [com.taoensso/tower "2.0.2"]
-                [markdown-clj "0.9.44"]
-                [environ "0.5.0"]
-                [noir-exception "0.2.2"]
-                [org.clojure/clojurescript "0.0-2665"]
+                [org.clojure/clojurescript "0.0-2342"]
                 [reagent "0.5.0-alpha"]]
 
- :repl-options {:init-ns word-dream.repl}
- :plugins [[lein-ring "0.8.10"]
-           [lein-environ "0.5.0"]
-           [lein-cljsbuild "1.0.3"]]
- :ring {:handler word-dream.handler/app
-        :init    word-dream.handler/init
-        :destroy word-dream.handler/destroy}
+ :plugins [[lein-cljsbuild "1.0.3"]]
+ :hooks [leiningen.cljsbuild]
+
+ :source-paths ["src"]
+
+ :profiles {:prod {:cljsbuild
+                    {:builds
+                      {:client {:compiler
+                                {:optimizations :advanced
+                                 :preamble ^:replace ["reagent/react.min.js"]
+                                 :pretty-print false}}}}}
+            :dev {:cljsbuild
+                  {:builds
+                   {:client {:compiler
+                             {:source-map "public/js/page.js.map"}}}}}}
 
  :cljsbuild
  {:builds
-  [{:id "dev"
-    :source-paths ["src-cljs"]
-    :compiler
-    {:optimizations :none
-     :output-to "resources/public/js/app.js"
-     :output-dir "resources/public/js/"
-     :pretty-print true
-     :source-map true}}
-   {:id "release"
-    :source-paths ["src-cljs"]
-    :compiler
-    {:output-to "resources/public/js/app.js"
-     ;:source-map "resources/public/js/app.js.map"
-     :preamble ["reagent/react.js"]
-     :optimizations :advanced
-     :pretty-print false
-     :output-wrapper false
-     :closure-warnings {:non-standard-jsdoc :off}}}]}
+  {:client {:source-paths ["src"]
+            :compiler
+            {:preamble ["reagent/react.js"]
+             :output-dir "public/js/"
+             :output-to "public/js/page.js"
+             :pretty-print true}}}})
 
- :profiles
- {:uberjar {:aot :all}
-  :release {:ring {:open-browser? false
-                      :stacktraces?  false
-                      :auto-reload?  false}}
-  :dev {:dependencies [[ring-mock "0.1.5"]
-                       [ring/ring-devel "1.3.0"]
-                       [pjstadig/humane-test-output "0.6.0"]]
-        :injections [(require 'pjstadig.humane-test-output)
-                     (pjstadig.humane-test-output/activate!)]
-        :env {:dev true}}}
- :min-lein-version "2.0.0")
+
+ ; :cljsbuild
+ ; {:builds
+ ;  {:builds
+ ;     {:client {:source-paths ["src"]
+ ;               :compiler
+ ;               {:preamble ["reagent/react.js"]
+ ;                :output-dir "target/client"
+ ;                :output-to "target/client.js"
+ ;                :pretty-print true}}}}
+ ;  {:dev  {:source-paths ["src-cljs"]
+ ;          :compiler
+ ;          {:optimizations :none
+ ;           :preamble ["reagent/react.js"]
+ ;           :externs ["reagent/externs/react.js"]
+ ;           :output-to "resources/public/js/app.js"
+ ;           :output-dir "resources/public/js/"
+ ;           :pretty-print true
+ ;           :source-map true}}
+ ;   :release {:source-paths ["src-cljs"]
+ ;             :compiler
+ ;             {:output-to "resources/public/js/app.js"
+ ;              ;:source-map "resources/public/js/app.js.map"
+ ;              :preamble ["reagent/react.js"]
+ ;              :externs ["reagent/externs/react.js"]
+ ;              :optimizations :advanced
+ ;              :pretty-print false
+ ;              :output-wrapper false
+ ;              :closure-warnings {:non-standard-jsdoc :off}}}}}
